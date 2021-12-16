38bc6b8fd7ee1c7879fecf2d26757dd2bc178df3
wisnupramoedya <wisnupramoedya@gmail.com>
2021-12-16 12:56:20 +0700

    fix: change port to 5000

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 app.register_blueprint(presences)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
